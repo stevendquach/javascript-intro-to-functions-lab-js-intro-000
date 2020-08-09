aa81e591e60f19254222d0a53bf40660aba5f49f
Steven Quach <stevendquach@gmail.com>
2020-08-09 02:55:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase) {
     return ("I can't hear you!")
-}
-  else if (string.toUpperCase) {
+} else if (string.toUpperCase) {
       return ("YES INDEED!")
-}
+    }
